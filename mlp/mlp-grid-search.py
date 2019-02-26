7d48d732375b788fd3d13b9fc8cd02812d7a9b49
Robert Kuramshin <robert@sensorscall.com>
2019-02-26 14:23:44 -0500

    getting started with multilayer perceptron model

---
+++ mlp/mlp-grid-search.py
@@ -0,0 +1,76 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler  
+
+scaler = StandardScaler()  
+
+#read data
+csv_data = pd.read_csv("../data/processed.csv")
+
+#random shuffle
+csv_data.sample(frac=1)
+
+#specify feature column names
+feature_cols = [
+"Radius A [ang]",
+"Radius B [ang]",
+"Formation energy [eV/atom]",
+"Volume per atom [A^3/atom]",
+"Goldschmidt Tolerance Factor",
+"A Electronegativity",
+"B Electronegativity",
+"C Electronegativity",
+"A Ionization Energy",
+"B Ionization Energy",
+"C Ionization Energy",
+]
+
+X = csv_data.loc[:, feature_cols]
+y = csv_data["Band gap [eV]"]
+
+# Split the dataset in two equal parts
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+
+scaler.fit(X_train)  
+
+X_train = scaler.transform(X_train)  
+
+X_test = scaler.transform(X_test)  
+
+tuned_parameters = [{"hidden_layer_sizes":[{5,},{10,},{25,},{50,},{4,2,},{8,3,}],"max_iter":[2000],"activation":["identity", "logistic", "tanh", "relu"],"learning_rate_init":[0.0001,0.001,0.01,0.1]}]
+
+
+clf = GridSearchCV(MLPRegressor(), tuned_parameters, cv=5)
+clf.fit(X_train, y_train)
+
+print("Best parameters set found on development set:")
+print()
+print(clf.best_params_)
+print()
+print("Grid scores on development set:")
+print()
+means = clf.cv_results_['mean_test_score']
+stds = clf.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+    print("%0.3f (+/-%0.03f) for %r"
+            % (mean, std * 2, params))
+print()
+
+print("Detailed classification report:")
+print()
+print("The model is trained on the full development set.")
+print("The scores are computed on the full evaluation set.")
+print()
+y_true, y_pred = y_test, clf.predict(X_test)
+print(mean_squared_error(y_true, y_pred))
+
+print()
+
