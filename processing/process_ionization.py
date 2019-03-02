bf8a0295e9c07b7bef8a6e0f71ff8d919141d38f
Robert Kuramshin <robert@sensorscall.com>
2019-03-02 16:26:57 -0500

    removing feactors related to X position atom

--- processing/process_ionization.py
+++ processing/process_ionization.py
@@ -1,32 +1,28 @@
 import pandas as pd
 
-pauling = pd.read_csv("data/ionization_energy.csv")
-dft = pd.read_csv("data/dft_improved2.csv")
+pauling = pd.read_csv("../data/ionization_energy.csv")
+dft = pd.read_csv("../data/dft.csv")
 
 a_vals = dft["A"]
 b_vals = dft["B"]
-c_vals = dft["C"]
 
 d = {}
 
 for index, row in pauling.iterrows():
     d[row["Element"]] = row["Energy"]
 
-en = pd.DataFrame({"A_IE":[0 for i in range(len(a_vals))],"B_IE":[0 for i in range(len(a_vals))],"C_IE":[0 for i in range(len(a_vals))]})
+en = pd.DataFrame({"A Ionization Energy":[0 for i in range(len(a_vals))],"B Ionization Energy":[0 for i in range(len(a_vals))]})
 
 count = 0
 for index, row in dft.iterrows():
     a = a_vals.iloc[index]
     b = b_vals.iloc[index]
-    c = c_vals.iloc[index]
-    en["A_IE"].iloc[index] = d[a]
-    en["B_IE"].iloc[index] = d[b]
-    en["C_IE"].iloc[index] = d[c]
+    en["A Ionization Energy"].iloc[index] = d[a]
+    en["B Ionization Energy"].iloc[index] = d[b]
 
 print(en)
 
-dft["A_IE"] = en["A_IE"]
-dft["B_IE"] = en["B_IE"]
-dft["C_IE"] = en["C_IE"]
-dft.to_csv("data/dft_improved3.csv")
+dft["A Ionization Energy"] = en["A Ionization Energy"]
+dft["B Ionization Energy"] = en["B Ionization Energy"]
+dft.to_csv("../data/dft_ionization_energy.csv")
     
--- processing/process_pauling.py
+++ processing/process_pauling.py
@@ -1,32 +1,28 @@
 import pandas as pd
 
-pauling = pd.read_csv("data/pauling_en.csv")
-dft = pd.read_csv("data/dft_improved.csv")
+pauling = pd.read_csv("../data/pauling_en.csv")
+dft = pd.read_csv("../data/dft_ionization_energy.csv")
 
 a_vals = dft["A"]
 b_vals = dft["B"]
-c_vals = dft["C"]
 
 d = {}
 
 for index, row in pauling.iterrows():
     d[row["Element"]] = row["EN"]
 
-en = pd.DataFrame({"A_EN":[0 for i in range(len(a_vals))],"B_EN":[0 for i in range(len(a_vals))],"C_EN":[0 for i in range(len(a_vals))]})
+en = pd.DataFrame({"A Electronegativity":[0 for i in range(len(a_vals))],"B Electronegativity":[0 for i in range(len(a_vals))]})
 
 count = 0
 for index, row in dft.iterrows():
     a = a_vals.iloc[index]
     b = b_vals.iloc[index]
-    c = c_vals.iloc[index]
-    en["A_EN"].iloc[index] = d[a]
-    en["B_EN"].iloc[index] = d[b]
-    en["C_EN"].iloc[index] = d[c]
+    en["A Electronegativity"].iloc[index] = d[a]
+    en["B Electronegativity"].iloc[index] = d[b]
 
 print(en)
 
-dft["A_EN"] = en["A_EN"]
-dft["B_EN"] = en["B_EN"]
-dft["C_EN"] = en["C_EN"]
-dft.to_csv("data/dft_improved2.csv")
+dft["A Electronegativity"] = en["A Electronegativity"]
+dft["B Electronegativity"] = en["B Electronegativity"]
+dft.to_csv("../data/dft_pauling_electronegativity.csv")
     
