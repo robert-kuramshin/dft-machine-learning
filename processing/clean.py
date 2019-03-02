b15ba11127d7c623490a226ced7675c26c9f9673
Robert Kuramshin <robert@sensorscall.com>
2019-03-02 16:51:21 -0500

    splitting data into test and train as part of cleanup

--- processing/clean.py
+++ processing/clean.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-csv_data = pd.read_csv("../data/dft_pauling_electronegativity.csv")
+csv_data = pd.read_csv("../data/processed.csv")
 
 feature_cols = [
 "Radius A [ang]",
@@ -23,4 +23,20 @@ for feature in feature_cols:
 
 csv_data = csv_data[csv_data["Band gap [eV]"] != "0"]
 
-csv_data.to_csv("../data/processed.csv")
+test_split_amount = 0.2 #20% of data is reserved for test
+
+csv_data = csv_data.sample(frac=1)
+
+#test train split
+length = csv_data.shape[0]
+train_size = int(length*(1-test_split_amount))
+
+train = csv_data.iloc[:train_size,:]
+test = csv_data.iloc[train_size:,:]
+
+print(train_size)
+print(train.shape[0])
+print(test.shape[0])
+
+train.to_csv("../data/train.csv")
+test.to_csv("../data/test.csv")
