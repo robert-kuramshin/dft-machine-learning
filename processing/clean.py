8029d4a47dd638ba2b85f8f125d906ae54e0d6e8
Robert Kuramshin <robert@sensorscall.com>
2019-03-17 15:41:18 -0400

    reprocessing data to fill missing rows with mean

--- processing/clean.py
+++ processing/clean.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-csv_data = pd.read_csv("../data/processed.csv")
+csv_data = pd.read_csv("../data/dft_pauling_electronegativity.csv")
 
 feature_cols = [
 "Radius A [ang]",
@@ -17,11 +17,17 @@ feature_cols = [
 "Tolerance Factor",
 ]
 
-#remove rows with missing feature values
-for feature in feature_cols:
-    csv_data = csv_data[csv_data[feature] != "-"]
-
+#do this first to make sure we don't set Nan bandgaps to zero
 csv_data = csv_data[csv_data["Band gap [eV]"] != "0"]
+
+#remove rows with missing feature values
+csv_data = csv_data.replace('-', np.nan)
+
+csv_data.dropna(subset=["Band gap [eV]"],inplace=True)
+
+for feature in feature_cols:
+    print(feature)
+    csv_data[feature].fillna(csv_data[feature].astype('float32').mean(),inplace=True)
 
 test_split_amount = 0.2 #20% of data is reserved for test
 
@@ -34,9 +40,11 @@ train_size = int(length*(1-test_split_amount))
 train = csv_data.iloc[:train_size,:]
 test = csv_data.iloc[train_size:,:]
 
-print(train_size)
 print(train.shape[0])
 print(test.shape[0])
+
+train = train.reset_index()
+test = test.reset_index()
 
 train.to_csv("../data/train.csv")
 test.to_csv("../data/test.csv")
