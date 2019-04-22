4f7a24faeaa6837566e3fcc0de733236a80716a6
Robert Kuramshin <robert.kuramshin98@gmail.com>
2019-04-22 09:45:58 -0400

    trying a more comprehensive grid search

--- gbr/gbr-grid-search.py
+++ gbr/gbr-grid-search.py
@@ -34,7 +34,21 @@ feature_cols = [
 'B Ionization Energy',
 'A Electronegativity',
 'B Electronegativity',
-'Goldschmidt Tolerance Factor'
+"A rs",
+"A rp",
+"A rd",
+"B rs",
+"B rp",
+"B rd",
+"B EA",
+"B s total",
+"B p total",
+"B d total",
+"B f total",
+"A g",
+"A p",
+"B g",
+"B p"
 ]
 
 
@@ -67,16 +81,20 @@ y_train = train["Band gap [eV]"]
 
 X_test = test.loc[:, feature_cols]
 y_test = test["Band gap [eV]"]
-tuned_parameters = [{'n_estimators': [100,250,500], 'max_depth': [4,8,12,16],'min_samples_leaf':[2,3,4], 'min_samples_split': [0.25,0.5,0.75,2,3,4],
-          'learning_rate': [0.01,0.05,0.1,0.2], 'loss': ['ls']}]
+tuned_parameters = [{'n_estimators': [1500,5000],
+                     'max_depth': [1,2,3,4,5,6,7,8],
+                     'min_samples_leaf':[1,2,3,4,5,6,7],
+                    'min_samples_split': [0.5,0.75,1.0,2,3,4],
+                    'learning_rate': [0.05,0.075,0.1,0.125],
+                'loss': ['ls','lad','huber']}]
 
-scores = ['neg_mean_squared_error', 'r2']
+scores = ['neg_mean_squared_error']
 
 for score in scores:
     print("# Tuning hyper-parameters for %s" % score)
     print()
 
-    clf = GridSearchCV(GradientBoostingRegressor(), tuned_parameters, cv=5,
+    clf = GridSearchCV(GradientBoostingRegressor(), tuned_parameters, cv=3, n_jobs=4,
                        scoring='%s' % score)
     clf.fit(X_train, y_train)
 
@@ -84,22 +102,22 @@ for score in scores:
     print()
     print(clf.best_params_)
     print()
-    print("Grid scores on development set:")
-    print()
-    means = clf.cv_results_['mean_test_score']
-    stds = clf.cv_results_['std_test_score']
-    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-        print("%0.3f (+/-%0.03f) for %r"
-              % (mean, std * 2, params))
-    print()
+    # print("Grid scores on development set:")
+    # print()
+    # means = clf.cv_results_['mean_test_score']
+    # stds = clf.cv_results_['std_test_score']
+    # for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+    #     print("%0.3f (+/-%0.03f) for %r"
+    #           % (mean, std * 2, params))
+    # print()
 
-    print("Detailed classification report:")
-    print()
-    print("The model is trained on the full development set.")
-    print("The scores are computed on the full evaluation set.")
-    print()
-    y_true, y_pred = y_test, clf.predict(X_test)
-    print(mean_squared_error(y_true, y_pred))
+    # print("Detailed classification report:")
+    # print()
+    # print("The model is trained on the full development set.")
+    # print("The scores are computed on the full evaluation set.")
+    # print()
+    # y_true, y_pred = y_test, clf.predict(X_test)
+    # print(mean_squared_error(y_true, y_pred))
     
     print()
 
