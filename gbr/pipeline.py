a6f3dde72aee0af217f2636e9f62a8735ccdc0b6
Robert Kuramshin <robert.kuramshin98@gmail.com>
2019-04-24 12:02:56 -0400

    adding correlation analysis

--- gbr/pipeline.py
+++ gbr/pipeline.py
@@ -14,7 +14,6 @@ from sklearn.decomposition import PCA
 from sklearn.metrics import r2_score
 
 ##################### 1. READ DATA #####################
-
 test = pd.read_csv("../data/test.csv")
 train = pd.read_csv("../data/train.csv")
 
@@ -96,7 +95,7 @@ feature_names = [
 ]
 
 ##################### 2. SPLIT INTO TEST AND TRAIN #####################
-
+print("Splitting")
 X_train = train.loc[:, feature_cols]
 y_train = train["Band gap [eV]"]
 
@@ -104,7 +103,7 @@ X_test = test.loc[:, feature_cols]
 y_test = test["Band gap [eV]"]
 
 ##################### 3. NORMALIZATION #####################
-
+print("Norm")
 scaler = StandardScaler()  
 scaler.fit(X_train)  
 X_train = scaler.transform(X_train)  
@@ -113,9 +112,10 @@ X_test = scaler.transform(X_test)
 X_train = pd.DataFrame(X_train)
 X_test = pd.DataFrame(X_test)
 
-##################### 4.5 COMPOUND FEATURES ################
+##################### 4.1 TRANSFORMED FEATURES ################
 #transforms
-transforms = [lambda x: np.power(x,1/2),lambda x: np.log(1+x),lambda x: np.exp(x)]
+print("Transforms")
+transforms = [lambda x: np.power(x,2),lambda x: np.power(x,3),lambda x: np.power(x,1/2),lambda x: np.log(1+x),lambda x: np.exp(x)]
 n_cols = len(X_train.columns)
 for col in range(n_cols):
     for trans in transforms:
@@ -130,14 +130,14 @@ for col in range(n_cols):
 
 
 n_cols = len(X_train.columns)
-
+##################### 4.2 COMPOSITE FEATURES ################
 
 #compounding
 count = 0
-print("Part 1")
+print("Compounding Part 1")
 for f_1 in range(n_cols):
     for f_2 in range(n_cols):
-        print(count," of ",n_cols * n_cols)
+        print(count," of ",n_cols * (n_cols-1))
         if (not f_1 == f_2):
             X_train[len(X_train.columns)] = 0 # add column
             X_train[len(X_train.columns)-1] = X_train.iloc[:,f_1] * X_train.iloc[:,f_2]
@@ -146,15 +146,28 @@ for f_1 in range(n_cols):
             count+=1
 
 #compounding
-count = 0
-print("Part 2")
-for f_1 in range(n_cols):
-    for f_2 in range(n_cols):
-        for f_3 in range(n_cols):
-            print(count," of ",n_cols * n_cols * n_cols)
-            if (not f_1 == f_2 and not f_2 == f_3 and not f_1 == f_3):
-                X_train[len(X_train.columns)] = 0 # add column
-                X_train[len(X_train.columns)-1] = X_train.iloc[:,f_1] * X_train.iloc[:,f_2] * X_train.iloc[:,f_3]
-                X_test[len(X_test.columns)] = 0 # add column
-                X_test[len(X_test.columns)-1] = X_test.iloc[:,f_1] * X_test.iloc[:,f_2] * X_test.iloc[:,f_3]
-                count+=1
+# count = 0
+# print("Part 2")
+# for f_1 in range(n_cols):
+#     for f_2 in range(n_cols):
+#         for f_3 in range(n_cols):
+#             print(count," of ",n_cols * n_cols * n_cols)
+#             if (not f_1 == f_2 and not f_2 == f_3 and not f_1 == f_3):
+#                 X_train[len(X_train.columns)] = 0 # add column
+#                 X_train[len(X_train.columns)-1] = X_train.iloc[:,f_1] * X_train.iloc[:,f_2] * X_train.iloc[:,f_3]
+#                 X_test[len(X_test.columns)] = 0 # add column
+#                 X_test[len(X_test.columns)-1] = X_test.iloc[:,f_1] * X_test.iloc[:,f_2] * X_test.iloc[:,f_3]
+#                 count+=1
+
+##################### 4.3 CORRELATION FILTER ################
+print("Correlation Filter")
+
+corr_matrix = X_train.corr()
+
+columns = np.full((corr_matrix.shape[0],), True, dtype=bool)
+for i in range(corr_matrix.shape[0]):
+    for j in range(i+1, corr_matrix.shape[0]):
+        if corr_matrix.iloc[i,j] >= 0.999:
+            if columns[j]:
+                columns[j] = False
+selected_columns = X_train.columns[columns]
