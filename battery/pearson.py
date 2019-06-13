cab32ebed6fcd9b7af14dac602e835f6cedb96f1
Robert Kuramshin <robert.kuramshin98@gmail.com>
2019-06-13 10:44:41 -0400

    removing correlated features

--- battery/pearson.py
+++ battery/pearson.py
@@ -9,5 +9,13 @@ y_test = pd.read_csv("res/y_test.csv",index_col=0)
 corr = X_train.corr()
 corr.to_csv("res/corr.csv")
 
-# X_test.to_csv("res/X_test_lasso.csv")
-# X_train.to_csv("res/X_train_lasso.csv")
+columns = np.full((corr.shape[0],), True, dtype=bool)
+for i in range(corr.shape[0]):
+    for j in range(i+1, corr.shape[0]):
+        if corr.iloc[i,j] >= 0.999:
+            if columns[j]:
+                columns[j] = False
+selected_columns = X_train.columns[columns]
+
+X_test[selected_columns].to_csv("res/X_test_corr.csv")
+X_train[selected_columns].to_csv("res/X_train_corr.csv")
