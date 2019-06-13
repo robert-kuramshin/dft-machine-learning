81e715eb62a392655f92ab6d1f7acdf26081ffae
robert-kuramshin <robert.kuramshin98@gmail.com>
2019-06-13 10:34:44 -0400

    fixing corr

--- battery/pearson.py
+++ battery/pearson.py
@@ -6,9 +6,7 @@ X_test = pd.read_csv("res/X_test_lasso.csv",index_col=0)
 y_train = pd.read_csv("res/y_train.csv",index_col=0)
 y_test = pd.read_csv("res/y_test.csv",index_col=0)
 
-csv_data = csv_data.loc[:, feature_cols]
-
-corr = df.corr()
+corr = X_train.corr()
 corr.to_csv("res/corr.csv")
 
 # X_test.to_csv("res/X_test_lasso.csv")
