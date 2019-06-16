768f17d0d40eb6dbf0f6616e2c35e13fef397fde
Robert Kuramshin <robert.kuramshin98@gmail.com>
2019-06-15 20:31:29 -0400

    reducing number of cross validation sections

--- battery/lasso.py
+++ battery/lasso.py
@@ -12,7 +12,7 @@ y_test = pd.read_csv("res/y_test.csv",index_col=0)
 
 
 tuned_parameters = [{'alpha': [0.0001,0.001,0.01,0.1,0.2,0.5],"max_iter":[10000000]}]
-las = GridSearchCV(linear_model.Lasso(), tuned_parameters, cv=10)
+las = GridSearchCV(linear_model.Lasso(), tuned_parameters, cv=5, n_jobs=3)
 las.fit(X_train, y_train)
 
 mse = mean_squared_error(y_test, las.predict(X_test))
