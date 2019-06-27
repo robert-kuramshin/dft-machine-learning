84ace1728224cd3a46086be23f6f524880bd9eed
Robert Kuramshin <robert.kuramshin98@gmail.com>
2019-06-27 14:45:15 -0400

    generic calc mse and r2

---
+++ battery/calc-mse.py
@@ -0,0 +1,12 @@
+#gbr based recursive feature elimination results
+import numpy as np
+import pandas as pd
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+in_y_pred = pd.read_csv("res/gbr_compound_res.csv")
+in_y_real = pd.read_csv("res/y_test.csv",index_col=0)
+
+print (mean_squared_error(in_y_real, in_y_pred))
+print (r2_score(in_y_real, in_y_pred))
